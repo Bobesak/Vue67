5ee310f99c6109f80bd0dd18a0b0e688882182a0
Bobes <robert.nadymacek@gmail.com>
2025-09-09 10:39:02 +0200

    initial
    hi

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header">
+    <p class="AuthorTitle">Bobes</p>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  width: 100%;
+  height: 20%;
+  background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
+  justify-content: center;
+}
+.AuthorTitle {
+  color: dimgray;
+  font-size: 200%;
+  margin: 15px;
+}
+</style>
