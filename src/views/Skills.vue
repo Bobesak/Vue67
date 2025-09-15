e4561a3fd207d131da17fb1ca55fc2026acf439b
Bobes <robert.nadymacek@gmail.com>
2025-09-15 11:04:19 +0200

    Skills Page expanded

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,52 +1,200 @@
 <script setup lang="ts">
-
+import { ref } from 'vue';
+const showCText = ref(false);
+const showUtext = ref(false);
+const showVtext = ref(false);
 </script>
 
 <template>
 <div class="MainBody">
-  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Nam quis nulla. Praesent vitae arcu tempor neque lacinia pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Mauris metus. Morbi scelerisque luctus velit. Sed convallis magna eu sem. Integer malesuada.</p>
-
-  <p>Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Fusce consectetuer risus a nunc. Aliquam ante. Aliquam in lorem sit amet leo accumsan lacinia. Curabitur sagittis hendrerit ante. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In rutrum.</p>
-
-  <p>Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Vivamus luctus egestas leo. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla est. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Curabitur bibendum justo non orci. Proin in tellus sit amet nibh dignissim sagittis. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Maecenas libero.</p>
-
-  <p>Phasellus faucibus molestie nisl. Et harum quidem rerum facilis est et expedita distinctio. Cras elementum. Praesent in mauris eu tortor porttitor accumsan. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Etiam dictum tincidunt diam. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu.</p>
-
-  <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Aliquam erat volutpat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam neque. Praesent in mauris eu tortor porttitor accumsan. Nullam at arcu a est sollicitudin euismod. Etiam dictum tincidunt diam. Integer in sapien. Aliquam erat volutpat. Nunc dapibus tortor vel mi dapibus sollicitudin. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nam quis nulla. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo.</p>
-
-  <p>Donec iaculis gravida nulla. Nulla non arcu lacinia neque faucibus fringilla. Fusce tellus. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Integer lacinia. Aenean id metus id velit ullamcorper pulvinar. Nam quis nulla.</p>
-
-  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Nam quis nulla. Praesent vitae arcu tempor neque lacinia pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Mauris metus. Morbi scelerisque luctus velit. Sed convallis magna eu sem. Integer malesuada.</p>
-
-  <p>Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Fusce consectetuer risus a nunc. Aliquam ante. Aliquam in lorem sit amet leo accumsan lacinia. Curabitur sagittis hendrerit ante. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In rutrum.</p>
-
-  <p>Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Vivamus luctus egestas leo. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla est. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Curabitur bibendum justo non orci. Proin in tellus sit amet nibh dignissim sagittis. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Maecenas libero.</p>
-
-  <p>Phasellus faucibus molestie nisl. Et harum quidem rerum facilis est et expedita distinctio. Cras elementum. Praesent in mauris eu tortor porttitor accumsan. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Etiam dictum tincidunt diam. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu.</p>
-
-  <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Aliquam erat volutpat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam neque. Praesent in mauris eu tortor porttitor accumsan. Nullam at arcu a est sollicitudin euismod. Etiam dictum tincidunt diam. Integer in sapien. Aliquam erat volutpat. Nunc dapibus tortor vel mi dapibus sollicitudin. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nam quis nulla. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo.</p>
-  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Nam quis nulla. Praesent vitae arcu tempor neque lacinia pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Mauris metus. Morbi scelerisque luctus velit. Sed convallis magna eu sem. Integer malesuada.</p>
-
-  <p>Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Fusce consectetuer risus a nunc. Aliquam ante. Aliquam in lorem sit amet leo accumsan lacinia. Curabitur sagittis hendrerit ante. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In rutrum.</p>
-
-  <p>Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Vivamus luctus egestas leo. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla est. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Curabitur bibendum justo non orci. Proin in tellus sit amet nibh dignissim sagittis. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Maecenas libero.</p>
-
-  <p>Phasellus faucibus molestie nisl. Et harum quidem rerum facilis est et expedita distinctio. Cras elementum. Praesent in mauris eu tortor porttitor accumsan. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Etiam dictum tincidunt diam. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu.</p>
-
-  <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Aliquam erat volutpat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam neque. Praesent in mauris eu tortor porttitor accumsan. Nullam at arcu a est sollicitudin euismod. Etiam dictum tincidunt diam. Integer in sapien. Aliquam erat volutpat. Nunc dapibus tortor vel mi dapibus sollicitudin. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nam quis nulla. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo.</p>
-
-
-  <p>Vivamus ac leo pretium faucibus. Fusce consectetuer risus a nunc. Aliquam ante. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Nullam eget nisl. Nunc dapibus tortor vel mi dapibus sollicitudin. Mauris tincidunt sem sed arcu. Curabitur bibendum justo non orci. Maecenas aliquet accumsan leo. Et harum quidem rerum facilis est et expedita distinctio. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Phasellus rhoncus. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? In convallis.</p></div>
+  <div class="main">
+    <header class="main-header">
+      Skills
+    </header>
+    <div class="main-content">
+      <div class="InfoModule">
+        <p class="Ctext" :class="{ visible: showCText }">
+          My First Language I learned and probably the one I enjoy working with the most.<br>
+          I'm still a beginner but I'm aspiring to work with C# in the future.
+        </p>
+        <div class="SkillLogo"
+             @mouseenter="showCText = true" @mouseleave="showCText = false">
+        </div>
+      </div>
+      <div class="InfoModule">
+        <p class="Utext" :class="{ visible: showUtext }">
+          Unity is the reason i got into coding originally and remains my hobby to this day. <br>
+          i love messing around and learning how the engine functions work, albeit a newbie im trying to learn
+        </p>
+        <div class="USkillLogo"
+             @mouseenter="showUtext = true" @mouseleave="showUtext = false">
+        </div>
+      </div>
+      <div class="InfoModule">
+        <p class="Vtext" :class="{ visible: showVtext }">
+          Vue.Js is the first and latest framework im trying to learn (as im typing this) and its my first step into the web world. <br>
+          Webpage making as a whole is new to me so im learning on the go and hopefully ill get good enough to make this web good looking :D
+        </p>
+        <div class="VSkillLogo"
+             @mouseenter="showVtext = true" @mouseleave="showVtext = false">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
-
 <style scoped>
-.MainBody {
+.MainBody
+{
   display: flex;
   flex-direction: column;
   width: 80%;
-  border-radius:30px;
-  background-color: greenyellow;
-  margin: 0 auto;
+  border-radius: 30px;
+  border-bottom: #444444 1px solid;
+  margin: 40px auto 0;
   text-wrap: wrap;
+
+  .SkillLogo {
+    min-width: 120px;
+    min-height: 120px;
+    width: 20%;
+    height: 20%;
+    background-image: url("src/assets/CSharp.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    outline: #444444 1px solid;
+    border-radius: 30px;
+    margin-left: 30px;
+    box-shadow: 2px 2px 8px #444444;
+  }  
+  .USkillLogo {
+    min-width: 120px;
+    min-height: 120px;
+    width: 20%;
+    height: 20%;
+    background-image: url("src/assets/Unity.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    outline: #444444 1px solid;
+    border-radius: 30px;
+    margin-left: 30px;
+    box-shadow: 2px 2px 8px #444444;
+  }
+  .VSkillLogo {
+    min-width: 120px;
+    min-height: 120px;
+    width: 20%;
+    height: 20%;
+    background-image: url("src/assets/vue.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    outline: #444444 1px solid;
+    border-radius: 30px;
+    margin-left: 30px;
+    box-shadow: 2px 2px 8px #444444;
+  }
+
+  .main-header {
+    margin-left: 10px;
+    font-size: 60px;
+    text-align: left;
+    border-top: #444444 1px solid;
+    border-radius: 30px;
+    padding-top: 15px;
+    padding-left: 10px;
+  }
+
+  .InfoModule {
+    justify-content: center;
+    display: flex;
+    flex-direction: row;
+    font-size: 25px;
+    align-items: center; /* zarovnat vertikálně */
+    gap: 20px; /* mezera mezi logem a textem */
+  }
+
+  .main-content {
+    margin-bottom: 40px;
+  }
+
+  /* Přidáno pro plynulé zobrazení textu bez poskakování */
+  .Ctext {
+    max-width: 60%;
+    font-size: 23px;
+
+    /* Text je vždy v layoutu, ale neviditelný */
+    opacity: 0;
+    visibility: hidden;
+
+    /* Přechod opacity */
+    transition: opacity 0.67s ease,
+    border-radius 0.67s ease;
+
+    /* Minimální šířka, aby text zabíral místo i když je neviditelný */
+    min-width: 270px; /* uprav podle potřeby */
+  }
+
+  /* Viditelný text */
+  .Ctext.visible {
+    border-radius: 30px;
+    border-top: #444444 1px solid;
+    border-bottom: #444444 1px solid;
+    padding: 10px;
+    opacity: 1;
+    visibility: visible;
+  } 
+  .Utext {
+    max-width: 60%;
+    font-size: 23px;
+
+    /* Text je vždy v layoutu, ale neviditelný */
+    opacity: 0;
+    visibility: hidden;
+
+    /* Přechod opacity */
+    transition: opacity 0.67s ease,
+    border-radius 0.67s ease;
+
+    /* Minimální šířka, aby text zabíral místo i když je neviditelný */
+    min-width: 270px; /* uprav podle potřeby */
+  }
+
+  /* Viditelný text */
+  .Utext.visible {
+    border-radius: 30px;
+    border-top: #444444 1px solid;
+    border-bottom: #444444 1px solid;
+    padding: 10px;
+    opacity: 1;
+    visibility: visible;
+  }
+  .Vtext {
+    max-width: 60%;
+    font-size: 23px;
+
+    /* Text je vždy v layoutu, ale neviditelný */
+    opacity: 0;
+    visibility: hidden;
+
+    /* Přechod opacity */
+    transition: opacity 0.67s ease,
+    border-radius 0.67s ease;
+
+    /* Minimální šířka, aby text zabíral místo i když je neviditelný */
+    min-width: 270px; /* uprav podle potřeby */
+  }
+
+  /* Viditelný text */
+  .Vtext.visible {
+    border-radius: 30px;
+    border-top: #444444 1px solid;
+    border-bottom: #444444 1px solid;
+    padding: 10px;
+    opacity: 1;
+    visibility: visible;
+  }
 }
 </style>
